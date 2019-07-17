e951f912bb61907e383f951c069adccca911fb7a
Felipe Nakao <felipe_nakao@hotmail.com>
2019-07-17 16:01:19 -0300

    Update index.js
    android update alert

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Alert, Linking } from 'react-native'
+import { Alert, Linking, Platform } from 'react-native'
 import DeviceInfo from 'react-native-device-info'
 import apisauce from 'apisauce'
 
-const createAPI = (baseURL = 'https://itunes.apple.com/br/') => {
-  const api = apisauce.create({
-    baseURL,
-    headers: {
-      'Cache-Control': 'no-cache',
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    timeout: 10000
-  })
+const createAPI = () => {
+  const baseURL = Platform.OS === 'ios' ?
+    'https://itunes.apple.com/br/' :
+    'https://s3.us-east-2.amazonaws.com/';
+  const api = Platform.OS === 'ios' ?
+    apisauce.create({
+      baseURL,
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      timeout: 10000
+    }) :
+    apisauce.create({ baseURL });
 
   return {
-    getLatest: (bundleId) => api.get('lookup', {bundleId})
+    getLatest: Platform.OS === 'ios' ? (bundleId) => api.get('lookup', {bundleId}) :
+      () => api.get('dev.rxpro.com.br/versionChecker.json')
   }
 }
 
@@ -27,14 +33,25 @@ const performCheck = () => {
   // Call API
   return api.getLatest(bundleId).then(response => {
     let latestInfo = null
-    // Did we get our exact result?
-    if (response.ok && response.data.resultCount === 1) {
-      latestInfo = response.data.results[0]
-      // check for version difference
-      updateIsAvailable = latestInfo.version !== DeviceInfo.getVersion()
+    if (Platform.OS === 'ios') {
+      // Did we get our exact result?
+      if (response.ok && response.data.resultCount === 1) {
+        latestInfo = response.data.results[0]
+        // check for version difference
+        updateIsAvailable = latestInfo.version !== DeviceInfo.getVersion()
+      }
+  
+      return {updateIsAvailable, ...latestInfo}
     }
 
-    return {updateIsAvailable, ...latestInfo}
+    // Did we get our exact result?
+    if (response.ok && response.data['com.rxpro.br']) {
+      latestInfo = response.data['com.rxpro.br'].minVersionName;
+      // check for version difference
+      updateIsAvailable = latestInfo !== DeviceInfo.getVersion()
+    }
+
+    return { updateIsAvailable, ...latestInfo, trackId: 'com.rxpro.br' }
   })
 }
 
@@ -42,14 +59,17 @@ const attemptUpgrade = (appId) => {
   // failover if itunes - a bit excessive
   const itunesURI = `itms-apps://itunes.apple.com/app/id${appId}?mt=8`
   const itunesURL = `https://itunes.apple.com/app/id${appId}?mt=8`
+  const marketURL = `market://details?id=${appId}`; 
 
-  Linking.canOpenURL(itunesURI).then(supported => {
-    if (supported) {
-      Linking.openURL(itunesURI)
-    } else {
-      Linking.openURL(itunesURL)
-    }
-  })
+  Platform.OS === 'ios' ? 
+    Linking.canOpenURL(itunesURI).then(supported => {
+      if (supported) {
+        Linking.openURL(itunesURI)
+      } else {
+        Linking.openURL(itunesURL)
+      }
+    }) :
+    Linking.openURL(marketURL);
 }
 
 const showUpgradePrompt = (appId) => {
@@ -58,7 +78,8 @@ const showUpgradePrompt = (appId) => {
     'Existe uma versão atualizada do aplicativo na App Store. Por favor, atualize.',
     [
       {text: 'Atualizar', onPress: () => attemptUpgrade(appId)},
-    ]
+    ],
+    { cancelable: false },
   )
 }
 
@@ -71,4 +92,3 @@ const promptUser = () => {
 export default {
   promptUser
 }
-
